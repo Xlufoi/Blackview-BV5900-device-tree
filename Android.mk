672add42cab50930005df132ae586c6ea112ae46
TemichX <pelmen.abemohis228@gmail.com>
2026-09-20 08:27:20 +0300

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for Blackview BV5900 (BV5900)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),BV5900)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Blackview BV5900 (BV5900)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_BV5900.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_BV5900-user \
+    omni_BV5900-userdebug \
+    omni_BV5900-eng
