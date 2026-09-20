a24acd6b9075593b510b9d9b3662648bde50d1b2
TemichX <pelmen.abemohis228@gmail.com>
2026-09-20 10:04:04 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_BV5900-user
-add_lunch_combo omni_BV5900-userdebug
-add_lunch_combo omni_BV5900-eng
+add_lunch_combo twrp_BV5900-user
+add_lunch_combo twrp_BV5900-userdebug
+add_lunch_combo twrp_BV5900-eng
